fbd9f4e76d312d5904b09a13548d6d7e66c31edd
Rok Z <roky.zz@gmail.com>
2017-03-26 00:36:14 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const VENDOR_LIBS = ['react', 'react-dom', 'react-bootstrap'];
+
+const config = {
+    entry: {
+        bundle: path.join(__dirname, 'src', 'index.js'),
+        vendor: VENDOR_LIBS
+    },
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({filename: 'style.css'})
+    ]
+}
+
+module.exports = config;
