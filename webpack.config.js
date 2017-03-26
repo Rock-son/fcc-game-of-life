8dbfd968ce6c415347cee845ee34928bfb8ad303
Rok Z <roky.zz@gmail.com>
2017-03-26 01:43:25 +0100

    corrected synthax

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ const config = {
                 exclude: /node_modules/
             },
             {
-                test: /\.scss/,
+                test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
                     use: ['css-loader', 'sass-loader']
