46279b498bf69f24935eb5146b6a5b43d9e5fa27
Rok Z <roky.zz@gmail.com>
2017-03-26 01:43:04 +0100

    added style.scss file import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Container from '/.components/appContainer';
-
+import Container from './components/appContainer';
+import './style.scss';
 
 document.addEventListener("DOMContentLoaded", function(event) {      
     ReactDOM.render(<Container/>, document.getElementById('root'));
