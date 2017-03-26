42acaefdc3d0889d53a7591c7f6ca9b00e95f406
Rok Z <roky.zz@gmail.com>
2017-03-26 01:13:54 +0100

    inital commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Container from '/.components/appContainer';
+
+
+document.addEventListener("DOMContentLoaded", function(event) {      
+    ReactDOM.render(<Container/>, document.getElementById('root'));
+}); 
